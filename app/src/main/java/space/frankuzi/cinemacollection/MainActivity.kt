cc1f22d35db01c691893b16f4ff385f66eb2c650
frankuzifr <wasabi555579@gmail.com>
2022-04-15 00:43:20 +0300

    Added film card view

--- app/src/main/java/space/frankuzi/cinemacollection/MainActivity.kt
+++ app/src/main/java/space/frankuzi/cinemacollection/MainActivity.kt
@@ -2,10 +2,26 @@ package space.frankuzi.cinemacollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import space.frankuzi.cinemacollection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        for (i in 0..5) {
+            val filmCard = layoutInflater.inflate(R.layout.film_view_card, null)
+            val filmName = filmCard.findViewById<TextView>(R.id.film_title_label)
+            filmName.text = i.toString()
+            binding.container.addView(filmCard)
+        }
     }
 }
