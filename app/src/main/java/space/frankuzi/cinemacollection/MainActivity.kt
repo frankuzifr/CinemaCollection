c08fb25947483d800c8985bde68a56151326c00c
frankuzifr <wasabi555579@gmail.com>
2022-04-16 18:03:43 +0300

    Reworked activity_main

--- app/src/main/java/space/frankuzi/cinemacollection/MainActivity.kt
+++ app/src/main/java/space/frankuzi/cinemacollection/MainActivity.kt
@@ -2,6 +2,7 @@ package space.frankuzi.cinemacollection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import space.frankuzi.cinemacollection.databinding.ActivityMainBinding
 
@@ -17,10 +18,16 @@ private lateinit var binding: ActivityMainBinding
         setContentView(view)
 
 
-        for (i in 0..5) {
+        for (i in 0..10) {
             val filmCard = layoutInflater.inflate(R.layout.film_view_card, null)
             val filmName = filmCard.findViewById<TextView>(R.id.film_title_label)
-            filmName.text = i.toString()
+            filmName.text = "Атака джентельменов"
+            val button =
+                filmCard.findViewById<com.google.android.material.card.MaterialCardView>(R.id.film_card)
+
+            button.setOnClickListener {
+                Log.i("", "HI")
+            }
             binding.container.addView(filmCard)
         }
     }
