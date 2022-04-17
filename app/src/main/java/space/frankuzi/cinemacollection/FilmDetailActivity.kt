618f74b3d890a34f950e9dc24495c072f0a374bc
frankuzifr <wasabi555579@gmail.com>
2022-04-17 20:11:34 +0300

    Added favourite checkbox, added translate information between activities

--- app/src/main/java/space/frankuzi/cinemacollection/FilmDetailActivity.kt
+++ app/src/main/java/space/frankuzi/cinemacollection/FilmDetailActivity.kt
@@ -1,43 +1,66 @@
 package space.frankuzi.cinemacollection
 
 import android.content.Intent
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract.Intents.Insert.ACTION
 import android.util.Log
-import space.frankuzi.cinemacollection.data.Film
 import space.frankuzi.cinemacollection.databinding.ActivityFilmDetailBinding
 import java.lang.Exception
 
 class FilmDetailActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityFilmDetailBinding
+    private lateinit var _binding: ActivityFilmDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityFilmDetailBinding.inflate(layoutInflater)
-        val view = binding.root
+        _binding = ActivityFilmDetailBinding.inflate(layoutInflater)
+        val view = _binding.root
         setContentView(view)
 
-        val imageIdRes = intent.getIntExtra("imageIdRes", 0)
-        val nameIdRes = intent.getIntExtra("nameIdRes", 0)
-        val descriptionIdRes = intent.getIntExtra("descriptionIdRes", 0)
-        val isFavourite = intent.getBooleanExtra("isFavourite", false)
+        val filmId = intent.getIntExtra(FILM_ID, 0)
+        val imageIdRes = intent.getIntExtra(IMAGE_ID_RES, 0)
+        val nameIdRes = intent.getIntExtra(NAME_ID_RES, 0)
+        val descriptionIdRes = intent.getIntExtra(DESCRIPTION_ID_RES, 0)
+        var isFavourite = intent.getBooleanExtra(IS_FAVOURITE, false)
 
-        binding.filmImage.setBackgroundResource(imageIdRes)
-        val detailToolbar = binding.detailToolbar
+        _binding.filmImage.setBackgroundResource(imageIdRes)
+        val detailToolbar = _binding.detailToolbar
         detailToolbar.setTitle(nameIdRes)
-        binding.filmDescription.setText(descriptionIdRes)
+        _binding.filmDescription.setText(descriptionIdRes)
 
         detailToolbar.setNavigationOnClickListener {
+            val intent = Intent()
+            intent.putExtra(FILM_ID, filmId)
+            intent.putExtra(IS_FAVOURITE, isFavourite)
+            intent.putExtra(COMMENT, _binding.comment.editText?.text.toString())
+            setResult(RESULT_OK, intent)
             finish()
         }
+
+        val favouriteItem = detailToolbar.menu.getItem(0)
+        favouriteItem.isChecked = isFavourite
+
+        favouriteItem.setIcon(
+            if (isFavourite) R.drawable.ic_baseline_favorite_24
+            else R.drawable.ic_baseline_favorite_border_24
+        )
 
         detailToolbar.setOnMenuItemClickListener {
             when (it.itemId) {
                 R.id.share -> onShareButtonClick(resources.getString(nameIdRes))
+                R.id.favourite -> {
+                    if (it.isChecked){
+                        it.isChecked = false
+                        isFavourite = false
+                        it.setIcon(R.drawable.ic_baseline_favorite_border_24)
+                    } else {
+                        it.isChecked = true
+                        isFavourite = true
+                        it.setIcon(R.drawable.ic_baseline_favorite_24)
+                    }
+                    Log.i("", it.isChecked.toString())
+                }
                 else -> throw Exception()
             }
             true
@@ -55,5 +78,14 @@ class FilmDetailActivity : AppCompatActivity() {
         val sharedIntent = Intent.createChooser(sendMessageIntent, null)
 
         startActivity(sharedIntent)
+    }
+
+    companion object {
+        const val FILM_ID = "film_id"
+        const val IMAGE_ID_RES = "imageIdRes"
+        const val NAME_ID_RES = "nameIdRes"
+        const val DESCRIPTION_ID_RES = "descriptionIdRes"
+        const val IS_FAVOURITE = "isFavourite"
+        const val COMMENT = "comment"
     }
 }
--- app/src/main/java/space/frankuzi/cinemacollection/MainActivity.kt
+++ app/src/main/java/space/frankuzi/cinemacollection/MainActivity.kt
@@ -12,16 +12,17 @@ import space.frankuzi.cinemacollection.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-private lateinit var binding: ActivityMainBinding
+    private lateinit var _binding: ActivityMainBinding
+    private val _requestCode: Int = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = _binding.root
         setContentView(view)
 
-        for (film in FilmsData.films) {
+        for ((index, film) in FilmsData.films.withIndex()) {
             val filmCard = layoutInflater.inflate(R.layout.film_view_card, null)
             val filmImage = filmCard.findViewById<ImageView>(R.id.film_image)
             filmImage.setBackgroundResource(film.imageIdRes)
@@ -33,14 +34,38 @@ private lateinit var binding: ActivityMainBinding
 
             button.setOnClickListener {
                 val intent = Intent(this, FilmDetailActivity::class.java)
-                intent.putExtra("imageIdRes", film.imageIdRes)
-                intent.putExtra("nameIdRes", film.nameIdRes)
-                intent.putExtra("descriptionIdRes", film.descriptionIdRes)
-                intent.putExtra("isFavourite", film.isFavourite)
-                startActivity(intent)
+                intent.putExtra(FilmDetailActivity.FILM_ID, index)
+                intent.putExtra(FilmDetailActivity.IMAGE_ID_RES, film.imageIdRes)
+                intent.putExtra(FilmDetailActivity.NAME_ID_RES, film.nameIdRes)
+                intent.putExtra(FilmDetailActivity.DESCRIPTION_ID_RES, film.descriptionIdRes)
+                intent.putExtra(FilmDetailActivity.IS_FAVOURITE, film.isFavourite)
+                startActivityForResult(intent, _requestCode)
             }
 
-            binding.container.addView(filmCard)
+            _binding.container.addView(filmCard)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode != _requestCode) {
+            super.onActivityResult(requestCode, resultCode, data)
+            return
+        }
+
+        if (resultCode != RESULT_OK)
+            return
+
+        if (data == null)
+            return
+
+        val filmId = data.getIntExtra(FilmDetailActivity.FILM_ID, 0)
+        val isFavourite = data.getBooleanExtra(FilmDetailActivity.IS_FAVOURITE, false)
+        val comment = data.getStringExtra(FilmDetailActivity.COMMENT)
+
+        FilmsData.films[filmId].isFavourite = isFavourite
+
+        Log.i("Film name", resources.getString(FilmsData.films[filmId].nameIdRes))
+        Log.i("Favourite", isFavourite.toString())
+        Log.i("Comment", comment.toString())
     }
 }
