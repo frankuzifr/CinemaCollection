61ca7bd1d57d3a32154edd8fef62aa8d300224d4
frankuzifr <wasabi555579@gmail.com>
2022-04-23 20:26:43 +0300

    Added localization

--- app/src/main/java/space/frankuzi/cinemacollection/data/Film.kt
+++ app/src/main/java/space/frankuzi/cinemacollection/data/Film.kt
@@ -1,9 +1,19 @@
 package space.frankuzi.cinemacollection.data
 
-class Film(
+import space.frankuzi.cinemacollection.R
+
+data class Film(
     val nameIdRes: Int,
     val descriptionIdRes: Int,
     val imageIdRes: Int,
     var isFavourite: Boolean = false,
     var isSelected: Boolean = false
-)
+){
+    val favouriteIconId: Int
+        get() {
+            return if (isFavourite)
+                R.drawable.ic_baseline_favorite_24
+            else
+                R.drawable.ic_baseline_favorite_border_24
+        }
+}
